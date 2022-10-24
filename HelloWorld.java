c631320aa086a246ac7cb3c2ca8d583160b76b07
P10r10 <70333265+P10r10@users.noreply.github.com>
2022-10-24 11:41:04 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-         System.out.println("Hello world V2.1");
+        System.out.println("Hello world V2.1");
+        System.out.println("Hello world V2.2");
     }
 
 }
